05a6f649ba48d8e2f3cea26d238b968e4117b658
Finble <elainefinn@hotmail.com>
2016-08-03 18:42:01 +0100

    added ability to get whole array of items + json saved to postman

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{ 
+    id: 1,
+    description: 'Meet mum for lunch',
+    completed: false // todo not yet done
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+},  {
+    id: 3,
+    description: 'Feed the cat',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
+
+// GET /todos  ==> to get collection / array of all items
+
+app.get('/todos', function (req, res) {
+   res.json(todos); 
+});
+
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
