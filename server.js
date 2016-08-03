33f83db0c4164cfe7d779579f3063a129f0f44d0
Finble <elainefinn@hotmail.com>
2016-08-03 21:42:00 +0100

    POST tweaked to ensure new data saved to array, tested GET to evidence data saved

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser'); // middleware
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-var todoNextId = 1; // a way to keep track of data stored locally, won't need when using a DB
+var todoNextId = 1; 
 
-app.use(bodyParser.json()); // enables middleware to be set up - anytime a json request comes in, express is going to parse it, and we can access it via request.body 
+app.use(bodyParser.json());  
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -15,14 +15,6 @@ app.get('/', function (req, res) {
 app.get('/todos', function (req, res) {
     res.json(todos); 
 });
-
-// GET /todos/:id ==> to get individual to do item - option 1:
-
-//app.get('/todos/:id', function (req, res) {
-//    res.send('Asking for todo with id of ' + req.params.id);
-//})
-
-// GET /todos/:id ==> to get individual to do item - option 2:
 
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10); // convert string to a number
@@ -45,7 +37,12 @@ app.get('/todos/:id', function (req, res) {
 
 app.post('/todos', function (req, res) {
     var body = req.body;
-    console.log('description: ' + body.description);
+    
+    // add id field
+    body.id = todoNextId++;
+    
+    // add to todo array
+    todos.push(body);
     res.json(body);
 });
 
