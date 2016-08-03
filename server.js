4745bb1a3a4517f5a08fd21b6ab5310ef7553862
Finble <elainefinn@hotmail.com>
2016-08-03 22:07:23 +0100

    added underscore module + implemented to enable refactoring of GET todos id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser'); // middleware
+var bodyParser = require('body-parser'); 
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,15 +17,27 @@ app.get('/todos', function (req, res) {
     res.json(todos); 
 });
 
+// underscore module has an automatice way of iterating through an array, using "where" - a list is searched until all values is found
+
+//_.where(listOfPlays, {author: "Shakespeare", year: 1611});
+//=> [{title: "Cymbeline", author: "Shakespeare", year: 1611},
+//    {title: "The Tempest", author: "Shakespeare", year: 1611}]
+
+// or can use findWhere, which finds one value:
+
+//_.findWhere(publicServicePulitzers, {newsroom: "The New York Times"});
+//=> {year: 1918, newsroom: "The New York Times"}
+
+
 app.get('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id, 10); // convert string to a number
-    var matchedTodo;
+    var todoId = parseInt(req.params.id, 10); 
+    var matchedTodo = _.findWhere(todos,{id: todoId}); // replaces code below
     
-    todos.forEach(function (todo) {
-       if (todoId === todo.id) {
-           matchedTodo = todo;
-       } 
-    });
+//    todos.forEach(function (todo) {
+//       if (todoId === todo.id) {
+//           matchedTodo = todo;
+//       } 
+//    });
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -33,15 +46,11 @@ app.get('/todos/:id', function (req, res) {
 });
 
 
-// POSTS can take data and store it in the array above
-
 app.post('/todos', function (req, res) {
     var body = req.body;
     
-    // add id field
     body.id = todoNextId++;
     
-    // add to todo array
     todos.push(body);
     res.json(body);
 });
