c2a65bd95caf33aaba7f197b1b517f194bab73b8
Finble <elainefinn@hotmail.com>
2016-08-08 11:24:54 +0100

    ran node command to result in new DB created, tweaked sequelize var so that new data file created is stored in playground folder vs root (by default)

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,11 +1,24 @@
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, { // creates a new instance of the 'blueprint' object Sequelize
 	'dialect': "sqlite",
-	'storage': 'basic-sqlite-database.sqlite'
+	'storage': __dirname + '/basic-sqlite-database.sqlite' // will now store data (once run node...) in the playground folder
 });
+
+// define a model (data)
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING
+	},
+	completed: {
+		type: Sequelize.BOOLEAN
+	}
+})
+
+// run node ./playground/basic-sqlite-database.js
+// output is individual SQL statements, that we would have had to write, but Sequelize did for us!
+// a new file has been created now in root, DELETE this file, so update line 4 (see above)
 
 sequelize.sync().then(function() {
 	console.log('Everything is synced');
 });
-
-// run node ./playground/basic-sqlite-database.js in terminal and if all working it should print 'Everything is synced'
