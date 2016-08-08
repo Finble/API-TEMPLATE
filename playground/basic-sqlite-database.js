9aa26ceee342f7facd32b8bd3e2d6baa767ddddd
Finble <elainefinn@hotmail.com>
2016-08-08 12:28:55 +0100

    added basic validations + tested by removing description + adding catch to handle + communicate error

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -5,28 +5,39 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 });
 
 // define a model (data)
+// for validations, look at sequelize docs, models, validation
 
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false, // adding validation, must complete description
+		validate: {
+			len: [1, 250]// only takes strings with length of 1 or greater than 1, not more than 250 characters
+		}
 	},
 	completed: {
-		type: Sequelize.BOOLEAN
+		type: Sequelize.BOOLEAN,
+		allowNull: false, // adding validation, must complete completed
+		defaultValue: false // adding validation, if someone doesn't complete a completed status (as not required by user), code will set to false
 	}
-})
+});
 
 // add a new to do item via Todo.create
 
-sequelize.sync({force: true}).then(function() { 
+sequelize.sync({
+	force: true
+}).then(function() { 
 	console.log('Everything is synced');
 
 	Todo.create({
-		description: 'Walk my dog',
+		// description: 'Walk my dog',  // taking out this throws error in terminal (now we have validations above)
 		completed: false
 	}).then(function (todo) { 
 		console.log('Finished!');
 		console.log(todo);
-	});
+	}).catch(function(e){  // can catch error and log out what is wrong (vs terminal throwing up error), by including catch function
+		console.log(e);
+	})
 });
 
 
