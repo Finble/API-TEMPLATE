fc27c2a30b350823d4b5a3a05dc916acf78ea361
Finble <elainefinn@hotmail.com>
2016-08-12 17:05:47 +0100

    Working API, CRUD + login/logout, tested on Postman + sqlbrowser on local API + Heroku

--- server.js
+++ server.js
@@ -155,19 +155,31 @@ app.post('/users', function (req, res) {
 
 // POST/users/login  (USER LOGIN)
 
-app.post('/users/login', function (req, res) {  
-    var body = _.pick(req.body, 'email', 'password');  
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    var userInstance; 
 
-    db.user.authenticate(body).then(function(user) {
+    db.user.authenticate(body).then(function (user) {
         var token = user.generateToken('authentication');
+        userInstance = user;  
 
-        if (token) {
-            res.header('Auth', token).json(user.toPublicJSON());
-        } else {
-            res.status(401).send();
-        }
-    }, function() {
+        return db.token.create({  
+            token: token
+        });
+    }).then(function (tokenInstance) {
+        res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+    }).catch(function () {
         res.status(401).send();
+    });
+});
+
+// DELETE/users/login (DELETE A USER INSTANCE)
+
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+    req.token.destroy().then(function() {  
+        res.status(204).send();
+    }).catch(function() {
+        res.status(500).send();
     });
 });
 
