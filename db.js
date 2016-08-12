a7f06f852b08d6caaf898841de60e3ca93ee9773
Finble <elainefinn@hotmail.com>
2016-08-12 17:29:16 +0100

    changed DB name to API-TEMPLATE in db.js

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ if (env === 'production') {
 } else {
 	 var sequelize = new Sequelize(undefined, undefined, undefined, { 
  	'dialect': "sqlite",
- 	'storage': __dirname + '/data/dev-todo-api.sqlite' 
+ 	'storage': __dirname + '/data/dev-api-template.sqlite' 
  });
 }
 
